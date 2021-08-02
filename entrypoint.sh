c24e20328f50965a195b03918718a1b085485112
Felipe Suero <felipesu19@github.com>
2021-08-02 11:10:28 -0400

    Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
+
+git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+git pull github ${GITHUB_REF} --ff-only
+
+cd $GITHUB_REPOSITORY
+
 jekyll build
+
+git add ...
+
+git commit -m "Automatic update"
+git push github HEAD:${GITHUB_REF}
