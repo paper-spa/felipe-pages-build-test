474e408713361fd0f13afeef8f16f7fa02e9ca43
Felipe Suero <felipesu19@github.com>
2021-08-02 11:16:30 -0400

    Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 
 git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 git pull github ${GITHUB_REF} --ff-only
-
+git config --global user.email "felipesu19@github.com"
+git config --global user.name "Your Name"
 cd $GITHUB_REPOSITORY
 
 jekyll build
