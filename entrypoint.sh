31f53d82065cba5fe881ee43086f06b6e18ab768
Felipe Suero <felipesu19@github.com>
2021-08-04 10:57:26 -0400

    Artifiact build

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,10 @@
 
 git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 git pull github ${GITHUB_REF} --ff-only
-git config --global user.email "felipesu19@github.com"
-git config --global user.name "Your Name"
 
-git fetch --all
-
-
-git checkout -b gh-pages
-git pull origin gh-pages
-git merge main --allow-unrelated-histories -X theirs
-
-cd hacker-master
 bundle install
-
 jekyll build
 
-ls -l
-cp -r _site/* ../docs/
+tar cf ${GITHUB_REPOSITORY}-${GITHUB_SHA}.tar $1
 
-git add -A
-
-git commit -m "Automatic update"
-git push origin gh-pages --force
+echo "::set-output name=artifact_sha::$GITHUB_SHA"
