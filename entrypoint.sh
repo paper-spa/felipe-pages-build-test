1c9ee8e922440b21757c23898e325a3d227cb3b2
Felipe Suero <felipesu19@github.com>
2021-08-04 11:00:45 -0400

    Artifiact build

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,9 @@ git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_RE
 git pull github ${GITHUB_REF} --ff-only
 
 bundle install
+cd $1
 jekyll build
+cd ..
 
 tar cf ${GITHUB_REPOSITORY}-${GITHUB_SHA}.tar $1
 
