ede037f6d979bbfbf5dfef07aa861607bd093761
Felipe Suero <felipesu19@github.com>
2021-08-19 11:13:48 -0400

    this can't be it

--- archive2.sh
+++ archive2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 REPO_NAME=$(basename $GITHUB_REPOSITORY)
 echo $(ls)
-tar -czfv ${REPO_NAME}_${GITHUB_SHA}.tar ./_site
+tar czfv ${REPO_NAME}_${GITHUB_SHA}.tar ./_site
 
 echo "::set-output name=artifact_name::${REPO_NAME}_${GITHUB_SHA}.tar"
